d06cea40db46ced3673771e375d964fffe9bcb54
arun ekambaram <arunvit1730@gmail.com>
2025-04-25 09:44:06 +0530

    Added Data Ingestion Config & Constants

---
+++ supplychain_ml/constants/training_pipeline/__init__.py
@@ -0,0 +1,28 @@
+import os
+import sys
+import numpy as np
+import pandas as pd
+
+
+
+"""
+Defining common constant variables for training pipeline
+"""
+TARGET_COLUMN = "is_late_s"
+PIPELINE_NAME: str = "supplychainml"
+ARTIFACT_DIR: str = "Artifacts"
+TRAIN_FILE_NAME: str = "train.csv"
+TEST_FILE_NAME: str =  "test.csv"
+FILE_NAME: str = "Supplychain.csv"
+
+
+"""
+Data Ingestion related constant start with DATA_INGESTION VAR NAME
+"""
+
+DATA_INGESTION_DIR_NAME: str = "data_ingestion"
+DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
+DATA_INGESTION_INGESTED_DIR: str = "ingested"
+
+
+
